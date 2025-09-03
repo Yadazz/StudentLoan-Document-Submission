3e41659bd6fb3295b07bfe98642a477398d9e960
Yadazz <pan080448@gmail.com>
2025-09-03 15:24:09 +0700

    เพิ่ม กยศ 102 สุดท้าย

--- SL/student/components/DocumentList.js
+++ SL/student/components/DocumentList.js
@@ -21,7 +21,10 @@ const DocumentList = ({ documents, uploads, uploadProgress, handleFileUpload, ha
             {/* แก้ไข: ใช้เงื่อนไขใหม่สำหรับแสดงปุ่มดาวน์โหลด */}
             {(doc.id === 'form_101' || doc.id === 'consent_student_form' 
             || doc.id === 'consent_father_form' || doc.id === 'consent_mother_form'
-            || doc.id === 'guardian_consent' || doc.id === 'guardian_income_cert' || doc.downloadUrl) && (
+            || doc.id === 'guardian_consent' || doc.id === 'guardian_income_cert' 
+            || doc.id === 'father_income_cert' || doc.id === 'mother_income_cert' 
+            || doc.id === 'single_parent_income_cert' || doc.id === 'famo_income_cert'
+            || doc.downloadUrl) && (
               <TouchableOpacity
                 onPress={() => handleDownloadDocument(doc.id, doc.downloadUrl)} // เรียกใช้ฟังก์ชันใหม่
                 style={styles.downloadButton}
