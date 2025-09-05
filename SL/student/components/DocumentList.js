78d4b09b938caac4db190a102d8a8f0021579e00
Yadazz <pan080448@gmail.com>
2025-09-05 15:49:13 +0700

    Update DocumentList.js เพิ่มรับรองสถานภาพ

--- SL/student/components/DocumentList.js
+++ SL/student/components/DocumentList.js
@@ -24,6 +24,7 @@ const DocumentList = ({ documents, uploads, uploadProgress, handleFileUpload, ha
             || doc.id === 'guardian_consent' || doc.id === 'guardian_income_cert' 
             || doc.id === 'father_income_cert' || doc.id === 'mother_income_cert' 
             || doc.id === 'single_parent_income_cert' || doc.id === 'famo_income_cert'
+            || doc.id === 'family_status_cert' || doc.id === 'family_status_required'
             || doc.downloadUrl) && (
               <TouchableOpacity
                 onPress={() => handleDownloadDocument(doc.id, doc.downloadUrl)} // เรียกใช้ฟังก์ชันใหม่
