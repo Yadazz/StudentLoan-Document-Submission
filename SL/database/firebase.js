9fbe6a46ef17c312ee9525d8bd46178a68ca1061
Chanad <98148689+Doaingn@users.noreply.github.com>
2025-09-14 20:26:05 +0700

    Update firebase.js

--- SL/database/firebase.js
+++ SL/database/firebase.js
@@ -11,13 +11,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDk3jscAWX7GZFhIkv6QNbDzmV8hvGOYfs",
-  authDomain: "studentloang8.firebaseapp.com",
-  projectId: "studentloang8",
-  storageBucket: "studentloang8.firebasestorage.app",
-  messagingSenderId: "761516049440",
-  appId: "1:761516049440:web:e6b5365a272fd39bb4e845",
-  measurementId: "G-71TWLLB2VM",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
@@ -35,3 +35,4 @@ export { auth };
 
 // Initialize Firebase Storage
 export const storage = getStorage(app);
+
